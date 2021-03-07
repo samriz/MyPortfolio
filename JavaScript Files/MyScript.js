1d427671887dc769f415c40c3d0ffa2b03256976
samriz <samriz@users.noreply.github.com>
2021-03-07 09:55:20 -0600

    Added folder "JavaScript Files". Added "MyScript.js" inside of it. Created function that dynamically creates a menu. Invoking function in all HTML files.

---
+++ JavaScript Files/MyScript.js
@@ -0,0 +1,24 @@
+
+
+function CreateMenu(links)
+{
+    var Menu = document.getElementsByClassName("Menu");
+    var aTags = new Array(links.length);
+    for(let i = 0; i < aTags.length; i++)
+    {
+        aTags[i] = document.createElement("a");
+        aTags[i].setAttribute("href", links[i]);
+        aTags[i].setAttribute("class", "MenuItem");
+        var position = links[i].search(".html");
+        var textnode = document.createTextNode(links[i].slice(0,position));
+        aTags[i].appendChild(textnode);                
+    }
+    for(let i = 0; i < Menu.length; i++)
+    {
+        for(let j = 0; j < aTags.length; j++)
+        {
+            Menu.item(i).appendChild(aTags[j]);
+            //aTags[j].appendChild(document.createElement("p"));
+        }            
+    }
+}

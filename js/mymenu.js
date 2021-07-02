62b25cc51e50b2894f704b807a784ecada1a8a5a
samriz <samriz@users.noreply.github.com>
2021-07-02 10:49:34 -0500

    added node_modules

--- js/mymenu.js
+++ js/mymenu.js
@@ -1,113 +1,62 @@
-    function CreateMenuAsync(links, extension)
+// import PopulateFileWithMenuAsync from "./modules/menuasync.js"
+
+function SetLink(link){return "./" + link;}
+
+function CreateMenuAsync(links, extension)
+{
+    var Menu = document.getElementsByClassName("menu");
+    var aTags = new Array(links.length);
+
+    //create anchor tags
+    for(let i = 0; i < aTags.length; i++)
     {
-        var Menu = document.getElementsByClassName("menu");
-        var aTags = new Array(links.length);
+        aTags[i] = document.createElement("a");
 
-        //create anchor tags
-        for(let i = 0; i < aTags.length; i++)
+        //set link here
+        //aTags[i].setAttribute("href", "./" + links[i]);
+        aTags[i].setAttribute("href", SetLink(links[i]));
+        aTags[i].setAttribute("class", "menuitem");
+        aTags[i].setAttribute("id", "menuitem" + i);
+        /*if(links[i] === "resume.html")
         {
-            aTags[i] = document.createElement("a");
+            aTags[i].setAttribute("target", "resume");
+        }*/
 
-            //set link here
-            //aTags[i].setAttribute("href", "./" + links[i]);
-            aTags[i].setAttribute("href", SetLink(links[i]));
-            aTags[i].setAttribute("class", "menuitem");
-            aTags[i].setAttribute("id", "menuitem" + i);
-            /*if(links[i] === "resume.html")
-            {
-                aTags[i].setAttribute("target", "resume");
-            }*/
-
-            var textnode;
-            if(links[i] === "index.html") 
-            {
-                textnode = document.createTextNode("sameer rizvi");
-            }
-            else 
-            {
-                var position = links[i].search(extension);
-                textnode = document.createTextNode(links[i].slice(0,position));
-            }
-            aTags[i].appendChild(textnode);                
-        }
-        //Menu.item(i).innerHTML += " ";
-
-        //place a vertical line between each menu item
-        for(let i = 0; i < Menu.length; i++)
+        var textnode;
+        if(links[i] === "index.html") 
         {
-            for(let j = 0; j < aTags.length; j++)
-            {
-                Menu.item(i).appendChild(aTags[j]);
-                if(j != aTags.length-1) Menu.item(i).innerHTML += " | ";
-            }            
+            textnode = document.createTextNode("sameer rizvi");
         }
-        return Promise.resolve(Menu);
+        else 
+        {
+            var position = links[i].search(extension);
+            textnode = document.createTextNode(links[i].slice(0,position));
+        }
+        aTags[i].appendChild(textnode);                
     }
+    //Menu.item(i).innerHTML += " ";
 
-    function CreateMenu(links, extension)
+    //place a vertical line between each menu item
+    for(let i = 0; i < Menu.length; i++)
     {
-        var Menu = document.getElementsByClassName("menu");
-        var aTags = new Array(links.length);
-        for(let i = 0; i < aTags.length; i++)
+        for(let j = 0; j < aTags.length; j++)
         {
-            aTags[i] = document.createElement("a");
-
-            //set link here
-            //aTags[i].setAttribute("href", "./" + links[i]);
-            aTags[i].setAttribute("href", SetLink(links[i]));
-            aTags[i].setAttribute("class", "menuitem");
-            aTags[i].setAttribute("id", "menuitem" + i);
-            /*if(links[i] === "resume.html")
-            {
-                aTags[i].setAttribute("target", "resume");
-            }*/
-
-            var textnode;
-            if(links[i] === "index.html") 
-            {
-                textnode = document.createTextNode("home");
-            }
-            else 
-            {
-                var position = links[i].search(extension);
-                textnode = document.createTextNode(links[i].slice(0,position));
-            }
-            aTags[i].appendChild(textnode);               
-        }
-        //Menu.item(i).innerHTML += " ";
-        for(let i = 0; i < Menu.length; i++)
-        {
-            for(let j = 0; j < aTags.length; j++)
-            {
-                Menu.item(i).appendChild(aTags[j]);
-                if(j != aTags.length-1) Menu.item(i).innerHTML += " | ";
-            }            
-        }
-        return Menu;
+            Menu.item(i).appendChild(aTags[j]);
+            if(j != aTags.length-1) Menu.item(i).innerHTML += " | ";
+        }            
     }
+    return Promise.resolve(Menu);
+}
 
-    function SetLink(link){return "./" + link;}
-
-    async function PopulateFileWithMenuAsync(extension)
+async function PopulateFileWithMenuAsync(extension)
+{
+    //var links = new Array("index", "resume", "about", "contact");
+    var links = new Array("index", "about", "contact");
+    for(var link in links)
     {
-        //var links = new Array("index", "resume", "about", "contact");
-        var links = new Array("index", "about", "contact");
-
-        for(var link in links)
-        {
-            links[link] += extension;
-        }
-        await CreateMenuAsync(links,extension); //waiting on Promise object
+        links[link] += extension;
     }
+    await CreateMenuAsync(links,extension); //waiting on Promise object
+}
 
-    function PopulateFileWithMenu(extension)
-    {
-        var links = new Array("index", "resume", "about", "contact");
-        for(var link in links)
-        {
-            links[link] += extension;
-        }
-        CreateMenu(links,extension);
-    }
-
-    PopulateFileWithMenuAsync(".html");
+PopulateFileWithMenuAsync(".html");
